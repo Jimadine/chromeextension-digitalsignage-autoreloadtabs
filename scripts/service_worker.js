65c664519aee592f22904e4e0ac83bcc1024d394
Jim Adamson <jim.adamson@york.ac.uk>
2023-05-18 10:53:42 +0100

    Significant changes to service worker JS:
    - restructure a lot of the code so as to avoid intermittent 'undefined' errors on variables
    - make reload fire AFTER the notification is closed (previously would happen when notification was shown)
    - use ternary operator to make conditionals more concise
    - remove unnecessary self-invoking function lines from top & bottom

--- scripts/service_worker.js
+++ scripts/service_worker.js
@@ -1,55 +1,64 @@
-(function() {
-    let browser, notificationSeconds, reloadIntervalMinutes
-    let alarmName = 'reloadAlarm' + (Math.floor(Math.random() * 1000)).toString()
-    if (navigator.userAgentData) {
-        let vendors = navigator.userAgentData.brands;
-        if (vendors.filter(e => e.brand === 'Google Chrome').length > 0) {
-            browser = 'Chrome'
-        } else {
-            browser = 'Chromium'
-        }
-    }
+let browser
+const alarmName = 'reloadAlarm-12345'
 
-    function setVars() {
-        chrome.storage.local.get(['reload_interval', 'notification_period'], function(result) {
-            reloadIntervalMinutes = parseInt(result.reload_interval) || 15;
-            notificationSeconds = parseInt(result.notification_period) || 5;
-            chrome.alarms.create(alarmName, {
-                'periodInMinutes': reloadIntervalMinutes
-            })
-        });
-    }
+if (navigator.userAgentData) {
+  let vendors = navigator.userAgentData.brands;
+  browser = vendors.filter(e => e.brand === 'Google Chrome').length > 0 ? 'Chrome' : 'Chromium'
+}
 
-    // Reload the vars any time the user clicks "Save"
-    chrome.storage.onChanged.addListener(function(changes, namespace) {
-        setVars()
-    });
-    setVars()
-
-    chrome.alarms.onAlarm.addListener(function(alarm) {
-        if (alarm.name == alarmName) {
-            let notificationId = (Math.floor(Math.random() * 1000)).toString()
-            const title = `${browser} tabs will be reloaded`
-            const options = {
-                body: `${browser} will reload all open tabs in ${notificationSeconds} seconds`,
-                icon: '../icons/refresh-white-48.png',
-                tag: notificationId,
-                requireInteraction: true
+chrome.alarms.onAlarm.addListener(function(alarm) {
+  chrome.storage.local.get(['reload_interval', 'notification_period'], function(r) {
+    if (alarm.name == alarmName) {
+      reloadIntervalMinutes = parseInt(r.reload_interval) || 15;
+      notificationSeconds = parseInt(r.notification_period) || 5;
+      const unit = reloadIntervalMinutes >= 2 ? 'minutes' : 'minute'
+      let notificationId = (Math.floor(Math.random() * 1000)).toString()
+      const title = `${browser} tabs will be reloaded`
+      const options = {
+        body: `${browser} will reload all open tabs in ${notificationSeconds} seconds (Reload interval is ${reloadIntervalMinutes} ${unit})`,
+        icon: '../icons/refresh-white-48.png',
+        tag: notificationId,
+        requireInteraction: true
+      }
+      self.registration.showNotification(title, options).then(() => new Promise(resolve => setTimeout(resolve, notificationSeconds * 1000))).then(() => self.registration.getNotifications()).then(notifications => {
+        const notification = notifications.find(notification => notification.tag === notificationId)
+        if (notification) {
+          notification.close()
+          chrome.tabs.query({}, function(tabs) {
+            for (var i = 0; i < tabs.length; i++) {
+              var tab = tabs[i]
+              chrome.tabs.reload(tab.id, {
+                bypassCache: true
+              })
             }
-            self.registration.showNotification(title, options).then(() => new Promise(resolve => setTimeout(resolve, notificationSeconds * 1000))).then(() => self.registration.getNotifications()).then(notifications => {
-                const notification = notifications.find(notification => notification.tag === notificationId)
-                if (notification) {
-                    notification.close()
-                }
-            })
-            chrome.tabs.query({}, function(tabs) {
-                for (var i = 0; i < tabs.length; i++) {
-                    var tab = tabs[i]
-                    chrome.tabs.reload(tab.id, {
-                        bypassCache: true
-                    })
-                }
-            })
+          })
         }
+      })
+    }
+  })
+})
+
+// Load the default values on extension installation
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.alarms.get(alarmName, (alarm) => {
+    // Alarms are remembered by the browser so check for existence before replacing
+    if (!alarm) {
+      chrome.storage.local.get(['reload_interval'], function(r) {
+        let reloadIntervalMinutes = parseInt(r.reload_interval) || 15;
+        chrome.alarms.create(alarmName, {
+          'periodInMinutes': reloadIntervalMinutes
+        })
+      })
+    }
+  })
+})
+
+// Reload the values any time the user clicks "Save"
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  chrome.storage.local.get(['reload_interval'], function(r) {
+    let reloadIntervalMinutes = parseInt(r.reload_interval) || 15;
+    chrome.alarms.create(alarmName, {
+      'periodInMinutes': reloadIntervalMinutes
     })
-})();
+  })
+})
