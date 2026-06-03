2eabbf17652a73fae750623113c95025f29b3125
Jim Adamson <jim.adamson@york.ac.uk>
2026-06-03 13:20:46 +0100

    Support disabling toast notifications altogether
    This is done by setting 'Notification period in seconds' to 0 in the Options page

--- scripts/service_worker.js
+++ scripts/service_worker.js
@@ -6,34 +6,46 @@ if (navigator.userAgentData) {
   browser = vendors.filter(e => e.brand === 'Google Chrome').length > 0 ? 'Chrome' : 'Chromium'
 }
 
-chrome.alarms.onAlarm.addListener(function(alarm) {
-  chrome.storage.local.get(['reload_interval', 'notification_period'], function(r) {
+chrome.alarms.onAlarm.addListener(function (alarm) {
+  chrome.storage.local.get(['reload_interval', 'notification_period'], function (r) {
     if (alarm.name == alarmName) {
       reloadIntervalMinutes = parseInt(r.reload_interval) || 15;
-      notificationSeconds = parseInt(r.notification_period) || 5;
+      notificationSeconds = r.notification_period !== undefined ? parseInt(r.notification_period) : 5;
       const unit = reloadIntervalMinutes >= 2 ? 'minutes' : 'minute'
-      let notificationId = (Math.floor(Math.random() * 1000)).toString()
-      const title = `${browser} tabs will be reloaded`
-      const options = {
-        body: `${browser} will reload all open tabs in ${notificationSeconds} seconds (Reload interval is ${reloadIntervalMinutes} ${unit})`,
-        icon: '../icons/refresh-white-48.png',
-        tag: notificationId,
-        requireInteraction: true
+      const reloadAllTabs = () => {
+        chrome.tabs.query({}, function (tabs) {
+          for (var i = 0; i < tabs.length; i++) {
+            var tab = tabs[i]
+            chrome.tabs.reload(tab.id, {
+              bypassCache: true
+            })
+          }
+        })
       }
-      self.registration.showNotification(title, options).then(() => new Promise(resolve => setTimeout(resolve, notificationSeconds * 1000))).then(() => self.registration.getNotifications()).then(notifications => {
-        const notification = notifications.find(notification => notification.tag === notificationId)
-        if (notification) {
-          notification.close()
-          chrome.tabs.query({}, function(tabs) {
-            for (var i = 0; i < tabs.length; i++) {
-              var tab = tabs[i]
-              chrome.tabs.reload(tab.id, {
-                bypassCache: true
-              })
+
+      if (notificationSeconds === 0) {
+        reloadAllTabs();
+      } else {
+        let notificationId = (Math.floor(Math.random() * 1000)).toString()
+        const title = `${browser} tabs will be reloaded`
+        const options = {
+          body: `${browser} will reload all open tabs in ${notificationSeconds} seconds (Reload interval is ${reloadIntervalMinutes} ${unit})`,
+          icon: '../icons/refresh-white-48.png',
+          tag: notificationId,
+          requireInteraction: true
+        }
+
+        self.registration.showNotification(title, options)
+          .then(() => new Promise(resolve => setTimeout(resolve, notificationSeconds * 1000)))
+          .then(() => self.registration.getNotifications())
+          .then(notifications => {
+            const notification = notifications.find(notification => notification.tag === notificationId)
+            if (notification) {
+              notification.close()
+              reloadAllTabs();
             }
           })
-        }
-      })
+      }
     }
   })
 })
@@ -54,14 +66,13 @@ chrome.runtime.onStartup.addListener(() => {
 })
 
 // Reload the values any time the user clicks "Save"
-chrome.storage.onChanged.addListener(function(changes, namespace) {
+chrome.storage.onChanged.addListener(function (changes, namespace) {
   setVars()
 })
 
 function setVars() {
-  chrome.storage.local.get(['reload_interval', 'notification_period'], function(r) {
+  chrome.storage.local.get(['reload_interval', 'notification_period'], function (r) {
     reloadIntervalMinutes = parseInt(r.reload_interval) || 15;
-    notificationSeconds = parseInt(r.notification_period) || 5;
     chrome.alarms.create(alarmName, {
       'periodInMinutes': reloadIntervalMinutes
     })
